b3b6678954aa0aa18645af1382d69942deb8eece
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-06-07 18:00:23 +0300

    Made a fix on the app.listen function and a change in the npm scripts.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require( "express");
+const express = require("express");
 const dotenv = require("dotenv");
-const ejs =  ("ejs");
+const ejs = "ejs";
 
 dotenv.config();
 
@@ -15,4 +15,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
