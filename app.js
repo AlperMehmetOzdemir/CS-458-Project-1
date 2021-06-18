c0894af975e00639ebf784091316ecde4bcf160a
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-06-18 16:33:18 +0300

    Added post route for login and checking if a user is registered. Added simple successful login page and simple unregistered user message

--- app.js
+++ app.js
@@ -7,43 +7,66 @@ dotenv.config();
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 
 const users = [
   {
     id: "12345",
-    password: "pass12345"
+    password: "pass12345",
   },
   {
     id: "20212021",
-    password: "pass2021"
+    password: "pass2021",
   },
   {
     id: "27",
-    password: "admin27"
+    password: "admin27",
   },
-]
+];
 
 // Index page
 app.get("/", (req, res) => {
-  res.render("index", {active: "login"});
+  res.render("index", { active: "login" });
 });
 
 // Login request
-app.post("/", (req,res) => {
-  let user = {id : req.body.id, password: req.body.password};
+app.post("/", (req, res) => {
+  let user = { id: req.body.bilkent_id, password: req.body.password };
 
   console.log(user);
 
-})
+  if (isUserRegistered(user)) {
+    res.status(200).render("login_success", { id: user.id, active: null });
+  } else {
+    res.status(401).send("You shall not pass");
+  }
+});
 
-app.get("/reset-password", (req,res) => {
-  res.render("reset_password", {active: "reset"});
-})
+app.get("/reset-password", (req, res) => {
+  res.render("reset_password", { active: "reset" });
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+function isUserRegistered(userToBeChecked) {
+  try {
+    if (
+      users.some(
+        (user) =>
+          user.id == userToBeChecked.id &&
+          user.password == userToBeChecked.password
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    throw new Error("[User] is not well defined");
+  }
+}
