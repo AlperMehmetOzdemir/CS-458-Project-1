4a6271b24e749b437a9deb8e9001c4324dc2dd9a
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-06-21 18:15:02 +0300

    SeleniumPlain for selenium WITHOUT JUnit5

--- SeleniumJavaTest/src/test/java/com/example/SeleniumJavaTest/SeleniumPlain.java
+++ SeleniumJavaTest/src/test/java/com/example/SeleniumJavaTest/SeleniumPlain.java
@@ -1,134 +1,52 @@
 package com.example.SeleniumJavaTest;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
+
+public class SeleniumPlain {
+
+     public static void main(String[] args){
+
+         final String WEB_URL = "https://srs-login-page.herokuapp.com/";
+         final String LOCAL_URL = "http://localhost:5000";
+
+         User[] registeredUsers = createRegisteredUsers();
+         User[] unregisteredUsersWithValidCredentials = createUnregisteredUsersWithValidCredentials();
+         User[] usersWithInvalidCredentials = createUsersWithInvalidCredentials();
+
+         System.setProperty("webdriver.chrome.driver", "D:/School/Summer 2021 CS-458/Projects/CS-458-Project-1/SeleniumJavaTest/chromedriver.exe");
+
+         WebDriver chromeDriver = new ChromeDriver();
+
+         chromeDriver.manage().window().maximize();
+
+        chromeDriver.get(LOCAL_URL);
+
+         System.out.println(chromeDriver.getTitle());
+
+         WebElement id_input = chromeDriver.findElement(By.cssSelector("input[name='bilkent_id']"));
+         WebElement password_input = chromeDriver.findElement(By.cssSelector("input[name='password']"));
+         WebElement login_button = chromeDriver.findElement(By.cssSelector("button"));
+
+         for (User user: registeredUsers) {
+             id_input.sendKeys(user.getBilkentId());
+             password_input.sendKeys(user.getPassword());
+             login_button.click();
+
+             Assertions.assertTrue(chromeDriver.findElement(By.cssSelector("section#loginSuccess")).isDisplayed(), String.format("Could not access login success page for %s", user.getBilkentId()));
+
+             chromeDriver.get("http://localhost:5000");
+         }
 
 
-public class SeleniumTest {
-
-    private String WEB_URL = "https://srs-login-page.herokuapp.com/";
-    private String LOCAL_URL = "http://localhost:5000";
-
-    private User[] registeredUsers = createRegisteredUsers();
-    private User[] unregisteredUsersWithValidCredentials = createUnregisteredUsersWithValidCredentials();
-    private User[] usersWithInvalidCredentials = createUsersWithInvalidCredentials();
 
 
+     }
 
-    private WebDriver[] webDrivers;
-
-
-    @BeforeAll
-    public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "D:/School/Summer 2021 CS-458/Projects/CS-458-Project-1/SeleniumJavaTest/chromedriver.exe");
-
-
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
-//        System.setProperty("webdriver.gecko.driver", "D:/School/Summer 2021 CS-458/Projects/CS-458-Project-1/SeleniumJavaTest/geckodriver.exe");
-//        System.setProperty("webdriver.opera.driver","D:/School/Summer 2021 CS-458/Projects/CS-458-Project-1/SeleniumJavaTest/operadriver.exe");
-    }
-
-    @BeforeEach
-    public void setUp() {
-        webDrivers = setupWebdrivers();
-    }
-
-    @Test
-    @DisplayName("TC-1 Check input validation")
-    @Disabled
-    public void inputValidationTest() {
-        /* TODO
-        *  Sadece chromedriver için:
-        *  Bütün registeredUsers için test et:
-        *       sonucunda succesful login sayfasına vardı mı diye kontrol et.
-        *  Bütün unregisteredUsersWithValidCredentials:
-        *       sonucunda text contenti "Wrong password or Bilkent ID number." olan bir "p.error_msg" WebElemant var mı diye kontrol et
-        *  Bütün usersWithInvalidCredentials için test et:
-        *       suncunda id veya password alanın altında hata mesajı çıkıtı mi diye kontrol et (yukarıdakine benzer şekilde).
-        *
-        * */
-
-
-    }
-
-    @Test
-    @DisplayName("TC-2 Check browser support")
-    public void browserSupportTest() {
-//        for (WebDriver driver: webDrivers )
-//        {
-            WebDriver driver = new ChromeDriver();
-
-
-            driver.manage().window().maximize();
-
-        driver.get("localhost:5000");
-
-        System.out.println(driver.getTitle());
-
-            WebElement id_input = driver.findElement(By.cssSelector("input[name='bilkent_id']"));
-            WebElement password_input = driver.findElement(By.cssSelector("input[name='password']"));
-            WebElement login_button = driver.findElement(By.cssSelector("button"));
-
-            for (User user: registeredUsers) {
-                id_input.sendKeys(user.getBilkentId());
-                password_input.sendKeys(user.getPassword());
-                login_button.click();
-
-                Assertions.assertTrue(driver.findElement(By.cssSelector("section#loginSuccess")).isDisplayed());
-
-                driver.get("http://localhost:5000");
-            }
-
-//        }
-    }
-
-    @Test
-    @DisplayName("TC-3 Check whether a captcha element is displayed after multiple failed login attempts")
-    @Disabled
-    public void multipleFailureCaptchaTest() {
-
-    }
-
-    @Test
-    @DisplayName("TC-4 Check page routing")
-    @Disabled
-    public void pageRoutingTest() {
-
-    }
-
-
-//    @Test
-//    @DisplayName("TC-5")
-//    public void inputValidationTest() {
-//
-//    }
-
-    @AfterEach
-    public void tearDown(){
-
-    }
-
-    @AfterAll
-    public static void tearDownAll(){
-
-    }
-
-    public WebDriver[] setupWebdrivers(){
-        WebDriver[] webDrivers = new WebDriver[3];
-
-        webDrivers[0] = new ChromeDriver();
-//        webDrivers[1] = new FirefoxDriver();
-//        webDrivers[2] = new OperaDriver();
-
-        return webDrivers;
-    }
-
-    public User[] createRegisteredUsers(){
+    public static User[] createRegisteredUsers(){
         User[] registeredUsers = new User[3];
 
         registeredUsers[0] = new User("20212021", "pass2021");
@@ -138,7 +56,7 @@ public class SeleniumTest {
         return registeredUsers;
     }
 
-    public User[] createUnregisteredUsersWithValidCredentials(){
+    public static User[] createUnregisteredUsersWithValidCredentials(){
         User[] unregisteredUsersWithValidCredentials = new User[21];
 
         // Off by a single character
@@ -175,7 +93,7 @@ public class SeleniumTest {
         return unregisteredUsersWithValidCredentials;
     }
 
-    public User[] createUsersWithInvalidCredentials(){
+    public static User[] createUsersWithInvalidCredentials(){
         User[] usersWithInvalidCredentials = new User[20];
 
         // Invalid bilkent_id and valid password
--- SeleniumJavaTest/src/test/java/com/example/SeleniumJavaTest/User.java
+++ SeleniumJavaTest/src/test/java/com/example/SeleniumJavaTest/User.java
@@ -17,4 +17,6 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+
 }
